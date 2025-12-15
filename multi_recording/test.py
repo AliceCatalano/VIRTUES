8084620464d97ffd653a50cf020c545406ffcc6a
Alice Catalano <91795156+AliceCatalano@users.noreply.github.com>
2025-12-15 09:29:46 +0100

    Revert "Add files via upload"

--- multi_recording/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import BleakClient
-
-address = "D1:81:23:02:83:E7"  # replace with your Shimmer MAC
-async def test():
-    async with BleakClient(address) as client:
-        print(await client.is_connected())
-
-asyncio.run(test())
